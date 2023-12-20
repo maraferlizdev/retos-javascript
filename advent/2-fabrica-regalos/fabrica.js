11503eb51832344e22512d1fb358f57a7a67cb61
Melanie Contreras <melanie.contreras@semarnat.gob.mx>
2023-12-20 10:24:08 -0600

    Primer intento regalos

---
+++ advent/2-fabrica-regalos/fabrica.js
@@ -0,0 +1,41 @@
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+manufacture(gifts, materials) // ["tren", "oso"]
+// // 'tren' SÍ porque sus letras están en 'tronesa'
+// // 'oso' SÍ porque sus letras están en 'tronesa'
+// // 'pelota' NO porque sus letras NO están en 'tronesa'
+
+
+const gifts2 = ['juego', 'puzzle']
+const materials2 = 'jlepuz'
+
+manufacture(gifts2, materials2) // ["puzzle"]
+
+const gifts3 = ['libro', 'ps5']
+const materials3 = 'psli'
+
+manufacture(gifts3, materials3) // []
+
+function manufacture(gifts, materials) {
+    let regalos=[]
+    let regalo=''
+    for(let gift in gifts){
+        regalo=''
+        for(i=0;i<gifts[gift].length;i++){
+            // console.log(i,gifts[gift][i]);
+            for(j=0;j<materials.length;j++){
+                // console.log(i,gifts[gift][i],gifts[gift][i]===materials[j],j,materials[j]);
+                if (gifts[gift][i]===materials[j] ) {
+                    regalo+=gifts[gift][i]
+                }
+            }
+        }
+        // console.log(regalo);
+       if(regalo===gifts[gift]){
+        regalos.push(gifts[gift])
+        
+       }
+    }
+     return console.log(regalos);
+}
